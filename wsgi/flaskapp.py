6bbcff384acf4ca5cb22aa80dc4032e3832817b2
rafael1193 <rafaelbailon@ieee.org>
2015-07-28 17:33:43 +0200

    Use a dynamic url to secure webhook

--- wsgi/flaskapp.py
+++ wsgi/flaskapp.py
@@ -12,16 +12,19 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 ieeebot.logger.debug(ieeebot.TOKEN)
 ieeebot.logger.debug(ieeebot.DATABASE_FILE)
 
-@app.route('/ieeetestbot', methods=['POST'])
-def hello():
-    update = request.get_json(force=True)
-    #ieeebot.logger.info(str(update))
-    
-    if update['update_id'] > ieeebot.last_update_id:
-        ieeebot.last_update_id = update['update_id']
-        ieeebot.process_update(update)
+@app.route('/webhook/<token>', methods=['POST'])
+def hello(token=None):
+    if token == ieeebot.TOKEN:
+        update = request.get_json(force=True)
+        ieeebot.logger.info(str(update))
+        
+        if update['update_id'] > ieeebot.last_update_id:
+            ieeebot.last_update_id = update['update_id']
+            ieeebot.process_update(update)
             
-    return "", 200
+        return "", 200
+    else:
+        return "", 400
 
 if __name__ == "__main__":
     app.run(debug = True) 
