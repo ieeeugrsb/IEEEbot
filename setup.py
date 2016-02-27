4ebf44b3c4af2546b9de69a7f6c9bddc229e2370
Rafael Bailón <rafaelbailon@ieee.org>
2016-02-27 01:26:07 +0100

    Version 0.4

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(name='IEEEbot',
-      version='0.3',
+      version='0.4',
       description='IEEE Telegram bot',
       author='IEEE Student Branch of Granada',
       author_email='ieeegranada@ieee.org',
       url='https://github.com/ieeeugrsb/IEEEbot',
       packages=find_packages(),
-      license='GPL3',
+      license='AGPLv3+',
       keywords='tools',
-      install_requires=['pyTelegramBotAPI', 'Flask>=0.7.2'],
+      install_requires=['pyTelegramBotAPI>=1.4.1', 'Flask>=0.7.2'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Programming Language :: Python :: 3',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv2)',
+          'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
       ]
       )
